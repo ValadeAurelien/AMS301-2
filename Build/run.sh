f2e43779459a628ed9e7ad0686c23f56a6e0839b
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-11-24 19:50:47 +0100

    ven. nov. 24 19:50:47 CET 2017

--- Build/run.sh
+++ Build/run.sh
@@ -4,16 +4,16 @@ nbTasks=2
 Wdir=benchmark
 output_file=log
 
-alpha=1
+alpha=0.1
 tol=1e-6
-maxit=1e3
-Ftype=1
-Farg=2
+maxit=1e5
+Ftype=0
+Farg=1
 meshFile=$Wdir/carre_64.msh
 outFFileName=$Wdir/solF
 outUFileName=$Wdir/solNum
-outUeFileName=$Wir/soleNamexa
-outEFileName=$Wdir/soleNamerr
+outUeFileName=$Wir/solExa
+outEFileName=$Wdir/solErr
 
 mkdir 2>/dev/null run_output
 rm 2>/dev/null -f run_output/$output_file
