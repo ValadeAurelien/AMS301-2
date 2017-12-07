9543283df8ee2d434f3b74d0de83f79455c7e260
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-12-07 16:02:32 +0100

    jeu. déc.  7 16:02:31 CET 2017

--- Build/.auxsubmitAlpha.sh
+++ Build/.auxsubmitAlpha.sh
@@ -3,7 +3,7 @@
 for i in $(seq 1 $logalphanb)
 do
     echo $i
-    alpha=$(calc "10^($logalpha(min+($logalphamax $logalphain*(-1))*$i/$logalphanb)" | sed 's/~//g')
+    alpha=$(calc "10^($logalphamin+($logalphamax $logalphamin*(-1))*$i/$logalphanb)" | sed 's/~//g')
     alpha=${alpha:0:5}
     make VERBOSE=0 && mpirun -np $nbTasks ./solver $alpha $tol $maxit $Ftype $Farg $solverType $saveMshs $meshFile $outFFileName $outUFileName $outUeFileName $outEFileName 
 done
