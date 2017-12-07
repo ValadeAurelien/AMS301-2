f15f96b7dd527c9115e71983900652feee386479
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-12-07 20:19:19 +0100

    jeu. déc.  7 20:19:19 CET 2017

--- Build/.auxsubmitTolL2.sh
+++ Build/.auxsubmitTolL2.sh
@@ -4,6 +4,6 @@ for i in $(seq 1 $logtolnb)
 do
     tol=$(python -c "print( 10**($logtolmin+($logtolmax $logtolmin*(-1))*$i/$logtolnb) )")
     tol=${tol:0:5}
-    echo $i $tol
+    echo "#tol $tol"
     make VERBOSE=0 && mpirun -np $nbTasks ./solver $alpha $tol $maxit $Ftype $Farg $solverType $saveMshs $meshFile $outFFileName $outUFileName $outUeFileName $outEFileName 
 done
